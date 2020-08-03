fd4e54d8ce399e4ce5daeb31ba7fc72d2be052d3
JustinRush80 <69156844+JustinRush80@users.noreply.github.com>
2020-08-03 10:35:30 -0400

    Add files via upload

---
+++ KNN_Preditor.py
@@ -0,0 +1,46 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+
+
+data = torch.load(r"C:\Users\JR100\OneDrive\Airfoil\Airfoil\data\naca4_simple_multiple_airfoils\data.pt")
+
+
+# data[0:6] recieves the airfoil type 2412
+x = np.concatenate([i.x for i in data[0:6]], axis=0 )
+y = np.concatenate([ i.y for i in data[0:6]],axis =0)
+pressure = np.concatenate([ i.pressure for i in data[0:6]], axis=0)
+
+
+
+# y will act as our input 
+x_train,x_test, y_train,y_test = train_test_split(y, pressure, test_size= .30, shuffle= True)
+
+
+
+
+# neighbors of 10 was arbitrarily choosen
+nn_r = KNeighborsRegressor(n_neighbors=10, weights="distance",  n_jobs=-1)
+pred= nn_r.fit(x_train,y_train).predict(x_test)
+
+
+plt.scatter(x_test,y_test, linewidths= 3, label= "Actual Pressure")
+plt.scatter (x_test,pred, color= "red", linewidths= .01, label="Predicted Pressure")
+
+plt.legend(["Actual Pressure","Predicted Pressure"])
+plt.xlabel("Y Component")
+plt.ylabel ("Pressure")
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
